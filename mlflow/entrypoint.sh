8dfd087a9f946f755589aae5b7fdf3bc7d2b08cb
bossebrutal <patrik.winkler@live.se>
2026-05-07 20:55:14 +0200

    fix: url-encode db password + använd ^|^ delimiter i set-env-vars

--- mlflow/entrypoint.sh
+++ mlflow/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
 
+# URL-encode password to handle special characters in the connection URI
+ENCODED_PASSWORD=$(python3 -c "import sys, urllib.parse; print(urllib.parse.quote(sys.argv[1], safe=''))" "${POSTGRES_PASSWORD}")
+
 exec mlflow server \
   --host 0.0.0.0 \
   --port "${PORT:-5000}" \
-  --backend-store-uri "postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/mlflow" \
+  --backend-store-uri "postgresql+psycopg2://${POSTGRES_USER}:${ENCODED_PASSWORD}@${POSTGRES_HOST}:5432/mlflow" \
   --artifacts-destination "gs://${MLFLOW_ARTIFACT_BUCKET}" \
   --serve-artifacts \
   --allowed-hosts '*'
